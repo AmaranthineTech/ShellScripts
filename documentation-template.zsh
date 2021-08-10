de30f6389fb3b293554a268395bfbdb1027a5e77
arunrpatwardhan <37834476+arunrpatwardhan@users.noreply.github.com>
2021-08-10 16:40:17 +0530

    Uploaded file
    This holds the template for the documentation typically found in scripts.

---
+++ documentation-template.zsh
@@ -0,0 +1,111 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------------------------------
+#NAME:		<#documentation#>
+#AUTHOR:	<#author name#>
+#CONTACT:	<#email#>
+#DATE:		<#date#>
+#WEBSITE:	<#url#>
+#-------------------------------------------------------------------------------------------------
+
+#LEGAL DISCLAIMER --------------------------------------------------------------------------------
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+#-------------------------------------------------------------------------------------------------
+
+#LICENSE/TERMS AND CONDITIONS --------------------------------------------------------------------
+#MIT License
+
+#Copyright (c) <#2021#> <#company name#>
+
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#-------------------------------------------------------------------------------------------------
+
+#ABOUT -------------------------------------------------------------------------------------------
+#This give basic information about the file such as 
+# <#filename#>
+# <#version#>
+#-------------------------------------------------------------------------------------------------
+
+#DESCRIPTION ------------------------------------------------------------------------------------- 
+# Here you will give a brief decription of what your script does. Here are some of the things that you can mention.
+# <#- Intended purpose#> 
+# <#- Situations when you can use this#>
+# <#- The target computers where this should ideally run on.#>
+#-------------------------------------------------------------------------------------------------
+
+#USAGE -------------------------------------------------------------------------------------------
+# This is where you give instructions on how to run the script. These could include:
+# <#- Command#>
+# <#- Available options#>
+# <#- Getting help#>
+#-------------------------------------------------------------------------------------------------
+
+#WARNING/CAUTION ---------------------------------------------------------------------------------
+#******************************************************************************************************************
+#******************************************************************************************************************
+#******************************************************************************************************************
+#******************************************************************************************************************
+# SOME SCRIPTS MAY MAKE SIGNIFICANT AND/OR IRREVERSIBLE CHANGES TO THE SYSTEM.
+# THIS IS A GOOD PLACE TO MENTION THOSE ACTIONS. TRY TO DAW THE READERS ATTENTION OUT HERE.
+#******************************************************************************************************************
+#******************************************************************************************************************
+#******************************************************************************************************************
+#******************************************************************************************************************
+#-------------------------------------------------------------------------------------------------
+
+#INSTALLATION ------------------------------------------------------------------------------------
+# Instructions for placing the script in the correct place are listed here. 
+# Location:		<#/Library/Scripts/#>
+# Permissions:	<#rwx r-x r-x#>
+#-------------------------------------------------------------------------------------------------
+
+#REQUIREMENTS ------------------------------------------------------------------------------------
+# Specify what is required
+# Shell:		<#/bin/zsh#>
+# OS:			<#macOS Big Sur 11.x#>
+# Dependencies:	<#Other tools#>
+#-------------------------------------------------------------------------------------------------
+
+#HELP/SUPPORT ------------------------------------------------------------------------------------
+# Give information about how users can get help about your script. This can onclude the use of help options or even man pages.
+# Mention how they could access the same.
+# For example:
+# You can get help by running the following commands.
+# <#<command> -h#>
+# <#<command> -help#>
+# OR
+# <#man <command>#>
+#-------------------------------------------------------------------------------------------------
+
+#HISTORY -----------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------
+# This is a good place to give version history. This helps the user of the script or any future modifiers of the script understand how the script evolved.
+#-------------------------------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------------------------
+# ------------------------------ SCRIPT STARTS HERE ----------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+
+
+
+
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+#-------------------------------------------------------------------------------------------------
+# ------------------------------ END OF SCRIPT ---------------------------------------------------
